9b0e3381330213e1d0cce6864995e25f017aea6f
roheendawlatzai <mahmadroain@gmail.com>
2021-08-03 14:26:14 +0430

    Fist Jenkins Commit

---
+++ src/com/jenkins/Main.java
@@ -0,0 +1,11 @@
+package com.jenkins;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i=0; i<5; i++){
+            System.out.println("Batch 9 Jekining");
+            Thread.sleep( 1000 );
+        }
+    }
+}
