3d56fd3543b1c1c0704683f8692b1d9d41a62aa7
roheendawlatzai <mahmadroain@gmail.com>
2021-08-06 10:14:30 +0430

    Jenkins Commit 4

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i=0; i<6; i++){
+        for (int i=0; i<9; i++){
             System.out.println("Batch 9 Jekining");
             Thread.sleep( 1000 );
         }
